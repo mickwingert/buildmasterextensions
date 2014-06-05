e7c6dcb7bef7b9fdb443394723e328c65be77fec
visualjazz-mwingert <mwingert@visualjazz.com.au>
2014-06-05 14:47:31 +1000

    + Added const class for Isobar tags of actions + Cleaned up code, added comments

--- IsobarExtension/Actions/XmlConfigTransformAction.cs
+++ IsobarExtension/Actions/XmlConfigTransformAction.cs
@@ -13,10 +13,12 @@ namespace IsobarExtension.Actions
     [ActionProperties(
         "Configuration Transforms",
         "Automatically run configuration transformations on all *.config files")]
-    [Tag("configuration-files, Isobar")]
+    [Tag(Tags.Isobar)]
+    [Tag(Inedo.BuildMaster.Tags.ConfigurationFiles)]
     [CustomEditor(typeof(XmlConfigTransformActionEditor))]
     public class XmlConfigTransformAction : AgentBasedActionBase
     {
+        #region Public Properties
         [Persistent]
         public string EnvironmentSuffix { get; set; }
 
@@ -33,8 +35,10 @@ namespace IsobarExtension.Actions
         /// Gets or sets a value indicating whether verbose logging should be enabled.
         /// </summary>
         [Persistent]
-        public bool Verbose { get; set; }
+        public bool Verbose { get; set; } 
+        #endregion
 
+        #region Public Methods
         protected override void Execute()
         {
             // see http://inedo.com/support/kb/1070/writing-an-agent-based-action for the list
@@ -43,19 +47,25 @@ namespace IsobarExtension.Actions
 
             LogInformation("Looking for any configuration transform files...");
 
+            // Get all the config files within the application
             var configFiles = EnumerateFilesRecursively(Context.SourceDirectory, new string[] { "*.config" });
+
+            LogInformation(String.Format("Found {0} config file(s)", configFiles.Count()));
 
             foreach (var configSourceFile in configFiles)
             {
                 var alreadyRun = new HashSet<string>();
 
+                // Apply any transforms where the suffix is Release.config
                 ApplyConfigTransforms(configSourceFile, "Release", fileOps, alreadyRun);
 
                 if (!String.IsNullOrWhiteSpace(EnvironmentSuffix))
                 {
+                    // Apply any transforms where the suffic is {EnvironmentSuffix}.config
                     ApplyConfigTransforms(configSourceFile, EnvironmentSuffix, fileOps, alreadyRun);
                 }
 
+                // Apply any addition transforms
                 foreach (var suffix in GetSuffixes(AdditionalTransforms))
                 {
                     ApplyConfigTransforms(configSourceFile, suffix, fileOps, alreadyRun);
@@ -65,9 +75,11 @@ namespace IsobarExtension.Actions
 
         public override string ToString()
         {
-            return "Writes all variables to variables.txt in the default directory on the remote agent.";
-        }
+            return "Apply config transforms on remote agent.";
+        } 
+        #endregion
 
+        #region Private Methods
         private void ApplyConfigTransforms(string sourceFile, string suffix, IFileOperationsExecuter fileOps,
                                           HashSet<string> alreadyRun)
         {
@@ -76,8 +88,10 @@ namespace IsobarExtension.Actions
                 suffix = suffix + ".config";
             }
 
+            // Update the extension of the found config to the suffix extensions
             string transformFile = Path.ChangeExtension(sourceFile, suffix);
 
+            // Try and find the transform config to apply to the source file
             if ((fileOps.FileExists(transformFile) &&
                  !string.Equals(sourceFile, transformFile, StringComparison.InvariantCultureIgnoreCase)) &&
                 !alreadyRun.Contains(transformFile))
@@ -87,17 +101,25 @@ namespace IsobarExtension.Actions
                 var transformExePath = Path.Combine(fileOps.GetBaseWorkingDirectory(),
                                                     @"ExtTemp\WindowsSDK\Resources\ctt.exe");
 
-                if(!fileOps.FileExists(transformExePath))
+                if (!fileOps.FileExists(transformExePath))
                     throw new FileNotFoundException("ctt.exe could not be found on the agent.", transformExePath);
 
+                // Get all the arguments that form the executable transform task
                 var arguments = BuildArguments(sourceFile, transformFile);
 
                 LogInformation("Performing XDT transform...");
 
+                // Call the transform executable for the file
                 ExecuteCommandLine(transformExePath, arguments);
             }
         }
 
+        /// <summary>
+        /// Build a list of command line arguments for config tranformation task
+        /// </summary>
+        /// <param name="sourceFile">The source file to be transformed</param>
+        /// <param name="transformFile">The transform file to be applied onto the source file</param>
+        /// <returns>An argument string</returns>
         private string BuildArguments(string sourceFile, string transformFile)
         {
             var buffer = new StringBuilder();
@@ -114,6 +136,12 @@ namespace IsobarExtension.Actions
             return buffer.ToString();
         }
 
+        /// <summary>
+        /// Recursively search through a directory or directories for files based on a search pattern
+        /// </summary>
+        /// <param name="parentDirectoryPath">The starting directory to recursively search</param>
+        /// <param name="searchPatterns">The search pattern to be applied</param>
+        /// <returns>An enumerable list of filenames</returns>
         private IEnumerable<string> EnumerateFilesRecursively(string parentDirectoryPath, params string[] searchPatterns)
         {
             var list = new List<string>();
@@ -127,9 +155,16 @@ namespace IsobarExtension.Actions
 
                 return list;
             }
+
+            // If no search pattern is defined, then return all files
             return Directory.EnumerateFiles(parentDirectoryPath, "*", SearchOption.AllDirectories);
         }
 
+        /// <summary>
+        /// Get all individual suffixes from a list of comma delimited suffixes
+        /// </summary>
+        /// <param name="suffixes">The list of suffixes to split</param>
+        /// <returns>A string list of suffixes</returns>
         private IEnumerable<string> GetSuffixes(string suffixes)
         {
             if (String.IsNullOrWhiteSpace(suffixes))
@@ -137,12 +172,14 @@ namespace IsobarExtension.Actions
                 return new string[0];
             }
 
+            // Split the suffix variable by comma and return as an array
             return
-                (from s in suffixes.Split(new char[] {','}) 
-                 select s.Trim() into s 
-                 where s.Length > 0 
+                (from s in suffixes.Split(new char[] { ',' })
+                 select s.Trim() into s
+                 where s.Length > 0
                  select s)
                     .ToArray<string>();
-        }
+        } 
+        #endregion
     }
 }
